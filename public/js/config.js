590724014d47976872fa611db09e300d88f52ba6
Dev <developer@macbookair.home>
2026-02-11 21:57:09 -0500

    Fix DigitalOcean deployment: Add frontend configuration file

---
+++ public/js/config.js
@@ -0,0 +1,6 @@
+// Auto-generated config
+window.YardGuardConfig = {
+    SUPABASE_URL: 'https://rjwaunghmcihpmockiap.supabase.co',
+    SUPABASE_ANON_KEY: 'sb_publishable_2wpXUJklTjkVJGAJnb5Wqg_6DG55FRO'
+};
+console.log('YardGuard Config Loaded (Secure)');
